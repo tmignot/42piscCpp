b19f2cd550909fbe944dde23683f442475c45569
Thomas MIGNOT <tmignot@e2r3p19.42.fr>
2015-01-14 11:32:07 +0100

    maj

--- j07/ex00/whatever.cpp
+++ j07/ex00/whatever.cpp
@@ -24,17 +24,28 @@ T			max(T a, T b)
 
 int			main(void)
 {
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
+	double a = 3465.65;
+	double b = 3465.65;
+	int c = 2;
+	int d = 3;
+	std::string e("toto");
+	std::string f("tata");
+
+	swap( a, b );
+	swap( c, d );
+	swap( e, f );
+
+	std::cout << "swap( a, b ) : a = " << a << ", b = " << b << std::endl;
+	std::cout << "swap( c, d ) : c = " << a << ", d = " << b << std::endl;
+	std::cout << "swap( e, f ) : e = " << a << ", f = " << b << std::endl;
+
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+
 	return 0;
 }
