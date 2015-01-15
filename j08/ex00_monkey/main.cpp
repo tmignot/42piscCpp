2aa7f2d3be821b13bba1957e2fbf76e720c53379
Jessy GIRARD <jgirard@e2r3p20.42.fr>
2015-01-15 15:53:53 +0100

    one word and I'm in

---
+++ j08/ex00_monkey/main.cpp
@@ -0,0 +1,29 @@
+#include "easyfind.hpp"
+
+int		main( void ) {
+	std::vector<int>	pouet;
+	std::list<int>		pouet2;
+
+	pouet.push_back(45);
+	pouet.push_back(40);
+	pouet.push_back(35);
+	pouet.push_back(30);
+	pouet2.push_back(45);
+	pouet2.push_back(42);
+	pouet2.push_back(40);
+	pouet2.push_back(35);
+
+	try {
+		easyfind< std::vector <int> >(pouet, 25);
+	} catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	try {
+		easyfind< std::list <int> >(pouet2, 25);
+	} catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
+}
