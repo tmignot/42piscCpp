82e44558f06abf320c6d873be3023bbc966904dc
Stephane HERMAN <stherman@e2r1p12.42.fr>
2015-01-15 17:00:51 +0100

    [ex02] ajout de la nested class iterator

--- j08/ex02/mutantstack.hpp
+++ j08/ex02/mutantstack.hpp
@@ -17,7 +17,7 @@ public:
 
 	};
 
-	bool empty(void) {
+	bool 	empty(void) {
 	    return (this->_root_node.empty());
 	};
 
@@ -25,16 +25,55 @@ public:
 	    return (this->_root_node.size());
 	};
 
-	void push(T data) {
+	void 	push(T data) {
 	    this->_root_node.push_front(data);
 	};
 
-	void pop(void) {
+	void 	pop(void) {
 	    this->_root_node.pop_front();
 	};
 
-	T top(void) {
+	T 		top(void) {
 	    return (this->_root_node.front());
+	};
+
+	class iterator : public std::iterator<std::input_iterator_tag, T> {
+		T 	*p;
+	public:
+		iterator(T *x) : p(x) {
+
+		};
+
+		iterator(const iterator &mit) : p(mit.p) {
+
+		};
+
+		iterator 	&operator++() {
+			++p;
+			return *this;
+		};
+
+		iterator 	operator++(T) {
+			iterator tmp(*this);
+			operator++();
+
+			return (tmp);
+		};
+
+		bool 		operator==(const iterator& rhs) {
+			return (p == rhs.p);
+		};
+
+		bool 		operator!=(const iterator& rhs) {
+			return (p != rhs.p);
+		};
+
+		int 		&operator*() {
+			return (*p);
+		};
+
+	private:
+
 	};
 
 private:
