4f881fd10d0b9ef5dab7d641b2593b312eb129fa
James WOODROW <jwoodrow@e2r3p19.42.fr>
2015-01-17 03:17:45 +0100

    added display selection in main via options

--- rush01/includes/GKrellM.hpp
+++ rush01/includes/GKrellM.hpp
@@ -1,4 +1,9 @@
 #ifndef GKRELLM_HPP
 # define GKRELLM_HPP
 
+#include <string>
+#include <iostream>
+#include "ShellDisplay.hpp"
+#include "WindowedDisplay.hpp"
+
 #endif
--- rush01/srcs/main.cpp
+++ rush01/srcs/main.cpp
@@ -1,6 +1,17 @@
 #include "GKrellM.hpp"
 
-int		main(void)
+int		main(int ac, char **av)
 {
+	IMonitorDisplay		*monitor = NULL;
+	std::string			mode;
+	
+	if (ac == 1 || (mode = std::string(av[1])) == "-s")
+		monitor = new ShellDisplay();
+	else if (ac >= 2 && mode == "-g")
+		monitor = new WindowedDisplay();
+	else
+		std::cout << "Invalid display mode option" << std::endl << "Please use -g or -s option to request a display mode" << std::endl;
+	if (monitor)
+		delete monitor;
 	return (0);
 }
