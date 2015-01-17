dbdae426b9a3f2803abff55c4ad82a6389a2aa00
James WOODROW <jwoodrow@e2r3p19.42.fr>
2015-01-17 01:11:20 +0100

    Module base

---
+++ rush01/includes/Module.hpp
@@ -0,0 +1,15 @@
+#ifndef MODULE_HPP
+# define MODULE_HPP
+
+class Module
+{
+	private:
+	protected:
+	public:
+		Module(void);
+		Module(Module const&);
+		~Module(void);
+		Module				&operator=(Module const&);
+};
+
+#endif
